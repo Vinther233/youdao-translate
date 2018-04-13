1d570fbdb8ce5a6b6d05ed2cfe360d597311d91d
JoJo_WEN <m18670825965@163.com>
2018-04-13 22:03:02 +0800

    更新

--- search_shell.sh
+++ search_shell.sh
@@ -1,6 +1,6 @@
 #/bin/env bash
-#一次查询一个单词，多个单词会单做句子翻译
-#参数中包含字母，且个数为1，就当做单词查询
+# 一次查询一个单词，多个单词会单做句子翻译
+# 参数中包含字母，且个数为1，就当做单词查询
 
 
 if [[ $@ =~ [a-z] ]]&&[[ $# == 1 ]];then
@@ -9,10 +9,10 @@ else
 	Input_key=$(echo $@| tr -d '\n' | xxd -plain | sed 's/\(..\)/%\1/g'|tr '[a-z]' '[A-Z]') #汉字URL编码
 fi
 
-PPRINT(){ #value是list，循环输出
+PPRINT(){ # value是list，循环输出
 	curl_command="curl -s http://fanyi.youdao.com/openapi.do?keyfrom=easysearch&\
 key=64700458&type=data&doctype=json&version=1.1&q=$Input_key"
-	Key_word=$($curl_command)  #返回的完整数据
+	Key_word=$($curl_command)  # 返回的完整数据
 	SEARCH_PATH=$(for argv in $@;do echo -n .$argv;done)
 	echo "$Key_word"|jq $SEARCH_PATH|egrep -v '\[|\]'|sed 's/[",]//g'
 }
